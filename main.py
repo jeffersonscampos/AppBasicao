f74bb08e9efc8f92c27dd90744ce1dfe4d4d58db
zitooo <zito.jau@gmail.com>
2019-04-01 12:46:32 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from flask import Flask, Request, response, redirect
+app = Flask(__name__)
+@app.route(‘/’)
+def main():
+ return redirect(‘static/index.html’)
---
+++ static/js/facebook.js
@@ -0,0 +1,55 @@
+var fbtoken = "";
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId: '158550007562379',
+		cookie: true,
+		xfbml: true,
+		version: 'v3.2'
+	});
+	FB.AppEvents.logPageView();
+
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+
+	FB.logout(function(response) {
+	});
+};
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {
+		return;
+	}
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "https://connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+})
+(document, 'script', 'facebook-jssdk');
+
+
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+	  statusChangeCallback(response);
+	});
+}
+
+function statusChangeCallback(response) {
+	console.log(response);
+	fbtoken = reponse.accessToken;
+	googletoken = "";
+	if (response.status === 'connected') {
+		testAPI();
+	  } else {
+	  }
+}
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+    	console.log(response);
+      	console.log('Successful login for: ' + response.name);
+    });
+  }
